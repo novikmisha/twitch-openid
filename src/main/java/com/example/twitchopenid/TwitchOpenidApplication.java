71f2f0665eca31952119bac2ae89161849833954
Mikhail Novik <novik.misha.1@gmail.com>
2023-05-01 23:50:50 +0300

    init commit

---
+++ src/main/java/com/example/twitchopenid/TwitchOpenidApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitchopenid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchOpenidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitchOpenidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/twitchopenid/TwitchOpenidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitchopenid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchOpenidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
